64cff0685a1f5bc2489fb4999f20c0af3987e563
Geno Ferollo <sakhib.orzklv@icloud.com>
2021-09-05 03:23:18 +0500

    new typescript

--- next.config.js
+++ next.config.js
@@ -5,37 +5,37 @@ const withPlugins = require("next-compose-plugins");
 const withFonts = require("nextjs-fonts");
 
 const mdxConfig = {
-  layoutPath: "layouts",
-  defaultLayout: true,
-  remarkPlugins: [
-    // require('remark-autolink-headings'),
-    require("remark-slug"),
-    require("remark-code-titles"),
-    require("remark-unwrap-images"),
-    // require('./utils/title-style')
-  ],
-  rehypePlugins: [],
-  extendFrontMatter: {
-    // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-    process: (mdxContent) => ({
-      wordCount: mdxContent.split(/\s+/gu).length,
-      readingTime: readingTime(mdxContent),
-    }),
-  },
+	layoutPath: "layouts",
+	defaultLayout: true,
+	remarkPlugins: [
+		// require('remark-autolink-headings'),
+		require("remark-slug"),
+		require("remark-code-titles"),
+		require("remark-unwrap-images"),
+		// require('./utils/title-style')
+	],
+	rehypePlugins: [],
+	extendFrontMatter: {
+		// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+		process: (mdxContent) => ({
+			wordCount: mdxContent.split(/\s+/gu).length,
+			readingTime: readingTime(mdxContent),
+		}),
+	},
 };
 
 module.exports = withPlugins([
-  withFonts,
-  withMdxEnhanced(mdxConfig),
-  {
-    env: {
-      apiKey: process.env.API_KEY,
-      authDomain: process.env.AUTH_DOMAIN,
-      databaseURL: process.env.DATABASE_URL,
-      projectId: process.env.PROJECT_ID,
-      messagingSenderId: process.env.MESSAGING_SENDER_ID,
-      appId: process.env.APP_ID,
-      apiUrl: process.env.API_URL,
-    },
-  },
+	withFonts,
+	withMdxEnhanced(mdxConfig),
+	{
+		env: {
+			apiKey: process.env.API_KEY,
+			authDomain: process.env.AUTH_DOMAIN,
+			databaseURL: process.env.DATABASE_URL,
+			projectId: process.env.PROJECT_ID,
+			messagingSenderId: process.env.MESSAGING_SENDER_ID,
+			appId: process.env.APP_ID,
+			apiUrl: process.env.API_URL,
+		},
+	},
 ]);
