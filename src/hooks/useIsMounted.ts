f2fe66e4ce3e70cdc9a73efe7872494cceb1b575
Geno Ferollo <sakhib.orzklv@icloud.com>
2021-09-05 03:25:59 +0500

    Update useIsMounted.ts

--- src/hooks/useIsMounted.ts
+++ src/hooks/useIsMounted.ts
@@ -2,6 +2,7 @@ import { useEffect, useRef } from "react";
 
 const useIsMounted = () => {
 	const isMounted = useRef(false);
+	// @ts-ignore
 	useEffect(() => {
 		isMounted.current = true;
 		return () => (isMounted.current = false);
