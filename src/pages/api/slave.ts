83ccc9b957eb6fdd5807ef977f4c9ad400ed733f
UwUssimo Robinson <uwussimo@icloud.com>
2021-11-10 04:08:38 +0500

    fixed so it redirects to the latest release

--- src/pages/api/slave.ts
+++ src/pages/api/slave.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async (req: NextApiRequest, res: NextApiResponse): Promise<any> => {
-	const link: string = "https://github.com/mad-maids/maid.slave/releases";
-	return res.redirect(link);
+	const latestLink = "https://api.github.com/repos/mad-maids/maid.slave/releases/latest";
+	const latestContent = await (await fetch(latestLink)).json();
+	return res.redirect(latestContent.html_url);
 };
